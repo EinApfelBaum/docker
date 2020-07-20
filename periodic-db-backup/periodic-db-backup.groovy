00e10600a904d730e59b95acd6df1abf6d6f4b60
EinApfelBaum <folke.b@web.de>
2020-07-20 20:28:54 +0200

    initial commit periodic backup

---
+++ periodic-db-backup/periodic-db-backup.groovy
@@ -0,0 +1,22 @@
+def branchNameExt;
+def dockerTag;
+def test;
+pipeline {
+  agent any
+  stages {
+    stage('Info') {
+        steps {
+            echo "Current dir: ${pwd()}"
+            sh 'docker --version'
+            sh 'git --version'
+            sh 'ls -la'
+          }
+        }
+    } 
+    // stage('Build and test') {
+    //   steps {
+    //     // sh 'docker build -t periodic-backup periodic-db-backup/'
+    //   }
+    // }   
+  }
+}
