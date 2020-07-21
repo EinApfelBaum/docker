42a85bac53a36063dd204d6c2cba15644aad0649
EinApfelBaum <folke.b@web.de>
2020-07-21 10:54:16 +0200

    update input

--- periodic-db-backup/periodic-db-backup.groovy
+++ periodic-db-backup/periodic-db-backup.groovy
@@ -12,6 +12,18 @@ pipeline {
             sh 'ls -la'
         }
     }
+    stage("Interactive_Input") {
+      steps {
+        script {
+          def userInput = input(
+            id: 'userInput', message: 'Enter remote registry:?', 
+            parameters: [
+            [$class: 'TextParameterDefinition', defaultValue: 'None', description: 'Remote address of registry.', name: 'Config']
+          ])
+          echo ("Remote: "+ ${userInput})
+        }
+      }
+    }
     stage('Build and test') {
        steps {
          sh 'docker build -t periodic-backup periodic-db-backup/'
