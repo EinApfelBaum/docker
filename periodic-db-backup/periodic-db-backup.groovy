2aa1e1be5d36e86ce890b7aaafa2a97a72ca4527
EinApfelBaum <folke.b@web.de>
2020-07-21 11:19:23 +0200

    update user input and information steps

--- periodic-db-backup/periodic-db-backup.groovy
+++ periodic-db-backup/periodic-db-backup.groovy
@@ -1,21 +1,11 @@
-def branchNameExt;
-def dockerTag;
-def test;
+def userInput;
 pipeline {
   agent any
   stages {
-    stage('Info') {
-        steps {
-            echo "Current dir: ${pwd()}"
-            sh 'docker --version'
-            sh 'git --version'
-            sh 'ls -la'
-        }
-    }
-    stage("Interactive_Input") {
+    stage("Interactive Input") {
       steps {
         script {
-          def userInput = input(
+          userInput = input(
             id: 'userInput', message: 'Enter remote registry:', 
             parameters: [
             [$class: 'TextParameterDefinition', defaultValue: 'None', description: 'Remote address of registry.', name: 'Config']
@@ -23,6 +13,25 @@ pipeline {
           echo ("Remote: ${userInput}")
         }
       }
+    }
+    stage('Prepare') {
+      parallel {
+        stage("Info") {
+          steps {
+            sh script "docker --version", label: "Docker Version"
+            sh script "git --version", label: "GitVersion"
+            echo "Current dir: ${pwd()}"
+            sh 'ls -la'
+            sh script "env", label: "Build Enviroment"
+          }
+        }  
+        stage("Docker images") {
+          steps{
+            sh script: "curl http://${userInput}/v2/_catalog", label: "Remote images"
+            sh script: "docker images", label: "Local images"
+          }
+        }
+      }        
     }
     stage('Build and test') {
        steps {
